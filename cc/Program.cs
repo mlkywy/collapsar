98ee0e4a08f530afe75b5f6aeabe41514e8c6acc
alina <mlkysheikh@gmail.com>
2023-02-11 19:15:55 -0500

    test

---
+++ cc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace cc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    return;
+                }
+
+                if (line == "1 + 2 * 3")
+                {
+                    Console.WriteLine("7");
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Invalid expression!");
+                }
+            }
+        }
+    }
+}
